1ff8bf0e4842a4ad52b91a6312aaab5211a8f032
Kiril Stas <vesground@gmail.com>
2020-07-17 19:08:10 +0300

    Add mocked data

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const profile = {
+  firstName: 'Jean',
+  firstName: 'Gonzales',
+  position: 'Product Owner',
+  photoUrl: 'public/disruptive-employee.jpg',
+  tasksCount: {
+    completed: 372,
+    open: 11
+  }
+};
+
+export const events = [
+  {
+    type: 'mark',
+    title: '',
+    createdAt: Date.now(),
+  },
+  {
+    type: 'mark',
+    title: '',
+    meta: {
+      target: {
+        title: ''
+      }
+    },
+    createdAt: Date.now(),
+  },
+  {
+    type: 'mark',
+    title: '',
+    meta: {
+      files: []
+    },
+    createdAt: Date.now(),
+  },
+]
