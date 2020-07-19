a44f0c138b31f14508dc625c97a54fb0de8287a4
Kiril Stas <vesground@gmail.com>
2020-07-19 19:48:56 +0300

    Add Tabs component

--- src/components/Tabs/Tab/index.js
+++ src/components/Tabs/Tab/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import classnames from 'classnames';
 
+import Text from 'components/Text';
+
 import './index.scss';
 
 const baseClass = 'app-component-tab';
 const Tab = ({ isActive, title, handleClick }) => {
   return (
     <div className={classnames(baseClass, { [`${baseClass}__active`]: isActive })} onClick={handleClick}>
-      {title}
+      <Text>{title}</Text>
     </div>
   );
 }
